a97aa7ac77fbba93da8ac5d3244d6ff03d12ac17
zaap38 <zappeurben@gmail.com>
2023-05-10 18:03:23 +0200

    switching to proportional output with clamp

--- mlp_output_guesser.py
+++ mlp_output_guesser.py
@@ -1,4 +1,4 @@
-#%%
+
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F 
@@ -14,7 +14,7 @@ import json
 import random as rd
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
-#%%
+
 class Data:
 
     def __init__(self, filename):
@@ -54,9 +54,10 @@ class Data:
 
         # output
         for item in range(3):
-            y.append(d["output"][0][item] / 10)
+            # y.append(d["output"][0][item] / 10)
+            y.append(d["output"][0][item] / d["input"]["amount"][item])
 
-        # reversed situation
+        # reversed situation -------------------------------------
         rx = []
         ry = []
         # input
@@ -68,8 +69,9 @@ class Data:
 
         # output
         for item in range(3):
-            ry.append((d["input"]["amount"][item] - d["output"][0][item]) / 10)
-        
+            # ry.append((d["input"]["amount"][item] - d["output"][0][item]) / 10)
+            ry.append((d["input"]["amount"][item] - d["output"][0][item]) / d["input"]["amount"][item])
+
         return x, y, rx, ry
     
     @staticmethod
@@ -117,201 +119,172 @@ class Data:
         for item in range(3):
             for i in range(11):
                 onehot_out.append(1 if i == d["output"][0][item] else 0)
-
-        # return torch.as_tensor(onehot_in, dtype=torch.float32), torch.as_tensor(onehot_out, dtype=torch.float32)
         return onehot_in , onehot_out
     
     def tensor_geter_x(self):
         return self.train_x
     def tensor_geter_y(self):
         return self.train_y
-#%%
+
+
 class Net(nn.Module):
     def __init__(self, input_size, output_size):
-        # super(Net, self).__init__()
-
-        # self.layers = []
-        # for i, s in enumerate(sizes):
-        #     if i > 0:
-        #         self.layers.append(nn.Linear(sizes[i - 1], s, bias=True))
-        # self.param = nn.ParameterList(self.layers)
         self.input_size  = input_size
         self.output_size = output_size
         super().__init__()
         self.fc1 = nn.Linear(input_size, 64)
-        # self.fc2 = nn.Linear(256, 128)
-        # self.fc3 = nn.Linear(128, 64)
         self.fc2 = nn.Linear(64, output_size)
     
     # forward pass 
     def forward(self, x):
-        # y = cp.deepcopy(x)
-        # for l in self.layers:
-        #     y = l(F.relu(y))
-        
-        # x = x.view(x.shape[0], -1)
-        # print(x.shape)
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc2(x))
         x = self.fc2(x)
-        # x = F.log_softmax(x, dim=self.output_size)
         
         return x
 
-#%%
 
-
-
-#%%
-data = Data("./src/data/parsed.json")
-#%%
-list_train_x = data.tensor_geter_x()
-list_train_y = data.tensor_geter_y()
-input_x = torch.from_numpy(np.array(list_train_x, dtype=np.float32))
-input_y = torch.from_numpy(np.array(list_train_y, dtype=np.float32))
-# input_x
-#%%
-input_x.shape
-#%%
-
-train_data = TensorDataset(input_x, input_y)
-# train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data,  batch_size=64)
-
-
-#%%    
-net = Net(9, 3)
-cross_entropy = nn.MSELoss()# nn.CrossEntropyLoss()
-optimizer=torch.optim.SGD(net.parameters(), lr=0.01 )
-bs = 20
-#%%
 def flat_accuracy(preds, labels):
     pred_flat = np.argmax(preds, axis=1).flatten()
     labels_flat = labels.flatten()
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+def reshape_labels(x, y):
+    for i in range(len(y)):
+        y[i] = reshape_label(x[i], y[i])
+    return y
+
+def reshape_label(x, y):
+    amounts = x[6:]
+    for i in range(len(y)):
+        y[i] = y[i] * amounts[i]
+    return y
+
+def reshape_pred(x, pred):
+    amounts = x[6:]
+    for i in range(len(pred)):
+        pred[i] = pred[i] * amounts[i]
+    return pred
 
 def test_loss(ins, labels):
     ins = torch.from_numpy(np.array(ins))
     labels = torch.from_numpy(np.array(labels))
     test_data = TensorDataset(ins, labels)
     test_dataloader = DataLoader(test_data,  batch_size=len(test_data))
+    cpt = 0
+    loss = 0
+    reward = 0  # for first talking participant
+    avg_reward = 0  # for both participants
+    label_reward = 0  # output reward for both participants
+    manhattan = 0
     for dl in test_dataloader:
         test_x, test_y = dl
         optimizer.zero_grad() 
-        # view as a column vector
-        # batch = tuple(t.cuda() for t in batch)
+
         b_input = test_x
         b_labels = test_y
         b_input = b_input.to(torch.float32)
         b_labels = b_labels.to(torch.float32)
-        # print(b_labels)
-        # start recording gradient
-        # inputs.requires_grad_()
-        # a forward pass on the network 
+
         y_pred = net(b_input)
-        preds.append
-        loss = cross_entropy(y_pred, b_labels)
-        display_index = rd.randint(0, 200)
+        y_pred = torch.nn.ReLU()(y_pred)
+        y_pred = torch.clamp(y_pred, min=0, max=1)
+        loss += loss_function(y_pred, b_labels)
 
-        total = 0
-        cpt = 0
-        for i, e in enumerate(y_pred):
-            for j in range(3):
-                total += abs(y_pred[i][j] - b_labels[i][j])
-                cpt += 1
-        total = total / cpt
+        rand_display = rd.randint(0, len(y_pred))
 
-        difference = []
-        for i in range(3):
-            difference.append(abs(y_pred[display_index].tolist()[i] - b_labels[display_index][i]))
-        pred = y_pred[display_index].tolist()
-        for i in range(len(pred)):
-            pred[i] = round(pred[i], 1)
-        print("========== Preview ==========")
-        print("Input:", b_input[display_index])
-        print("Output:", b_labels[display_index])
-        print("Pred:", pred)
-        print("Total:", total)
-        # print(loss)
+        for j in range(len(y_pred)):
+            cpt += 1
+
+            # label = cp.deepcopy(reshape_label(b_input[j], b_labels[j]))
+
+            mirror_pred = []
+            mirror_label = []
+            preds = [round(e, 1)  for e in y_pred[j].tolist()]  # reshape_pred(b_input[j], y_pred[j])
+            output = [round(e, 1)  for e in b_labels[j].tolist()]
+            input_v = [round(e, 1)  for e in b_input[j].tolist()]
+            
+            for k in range(len(preds)):
+                mirror_pred.append(round(1 - preds[k], 1))
+                mirror_label.append(round(1 - output[k], 1))
+
+            if j == rand_display:
+                print("Input:", input_v)
+                print("Output:", output)
+                print("Pred:", preds)
+                print("Mirror pred:", mirror_pred)
+
+            value_pred = reshape_pred(input_v, preds)
+            value_mirror = reshape_pred(input_v, mirror_pred)
+            value_output = reshape_pred(input_v, output)
+            value_mirror_out = reshape_pred(input_v, mirror_label)
+
+            for k in range(len(preds)):
+                reward += input_v[k] * value_pred[k] * 100
+                avg_reward += input_v[k] * value_pred[k] * 0.5 * 100 + input_v[k + 3] * value_mirror[k] * 0.5 * 100
+                label_reward += input_v[k] * value_output[k] * 0.5 * 100 + input_v[k + 3] * value_mirror_out[k] * 0.5 * 100
+
+                manhattan += abs(output[k] * 10 - preds[k] * 10)
+
+        print("Rwd:", round(reward / cpt, 2),
+              "Avg_rwd:", round(avg_reward / cpt, 2),
+              "Label_rwd:", round(label_reward / cpt, 2),
+              "Dist:", round(manhattan / cpt, 2))
+
         return loss
 
 
-#%%
-for epoch in range(50000):
+if __name__ == "__main__":
 
-    # keep track of the loss, error and the number of batches 
-    current_loss  = 0
-    current_error = 0
-    num_batches = 0 
-    acc_flag = 0
-    preds = []
-    
-    avg_loss = 0
-    
-    for batch in train_dataloader:
-        
-        # ---> forward pass 
-        # reset the gradient 
-        optimizer.zero_grad() 
-        # view as a column vector
-        # batch = tuple(t.cuda() for t in batch)
-        b_input,  b_labels = batch
-        b_input = b_input.to(torch.float32)
-        b_labels = b_labels.to(torch.float32)
-        # print(b_labels)
-        # start recording gradient
-        # inputs.requires_grad_()
-        # a forward pass on the network 
-        y_pred = net(b_input)
-        preds.append
-        loss = cross_entropy(y_pred, b_labels)
-        # print(loss)
-        avg_loss += loss
-        
-        optimizer.zero_grad()
-        loss.backward()
-        # update weights
-        optimizer.step()
-        
-        logits = y_pred.detach().cpu().numpy()
-        b_labels = b_labels.detach().cpu().numpy()
-        accuracy = flat_accuracy(logits, b_labels)
-        acc_flag+=accuracy
-    
-    if (epoch + 1) % 500 == 0:
-        avg_loss_epoch = avg_loss / len(train_dataloader)
-        loss_test = test_loss(data.test_x, data.test_y)
-        print(epoch + 1, avg_loss_epoch, loss_test)
-        
-    # print("Train loss: {}".format(tr_loss/nb_tr_steps))
-    # print("Train acc: {}".format(acc_flag))
-        # acc = (torch.argmax(y_pred, 1) == torch.argmax(b_labels, 1)).float().mean()
-        # print(f"Epoch {epoch} validation: Cross-entropy={float(loss)}, Accuracy={float(acc)}")
-        # compute the cross entropy loss 
-        # loss = cross_entropy(y_hat, label_minibatch)
-        # # <--- back propagation
-        # loss.backward()
-        # # update the net parameters 
-        # optimizer.step() 
-        # # get the current error 
-        # error = utils.get_error( y_hat.detach() , label_minibatch)
-        
-        # # update the running stats. 
-        # num_batches += 1 
-        # current_loss += loss.detach().item()
-        # current_error += error.item()
-    
-    # average loss/error over minibatches for the current epoch 
-    # avg_loss = current_loss / num_batches
-    # avg_error = current_error / num_batches
-    # elapsed_time = time.time() - start 
+    net = Net(9, 3)
+    loss_function = nn.MSELoss() # custom_loss_no_negative  # nn.MSELoss()
+    optimizer=torch.optim.SGD(net.parameters(), lr=0.01 )
+    bs = 20
 
-    # every 10 epochs display stats.
-    # if epoch % 10 == 0: 
-    #     print('The loss for epoch number ' + str(epoch) + ' = ' + str(avg_loss))
-    #     print('The error for epoch number ' + str(epoch) + ' = ' + str(avg_error))
+    data = Data("./src/data/parsed.json")
+    list_train_x = data.tensor_geter_x()
+    list_train_y = data.tensor_geter_y()
+    input_x = torch.from_numpy(np.array(list_train_x, dtype=np.float32))
+    input_y = torch.from_numpy(np.array(list_train_y, dtype=np.float32))
+    # input_y = reshape_labels(input_x, input_y)
 
-    #     # evaluate error on test set
-    #     error_on_test_set()
-# %%
+    train_data = TensorDataset(input_x, input_y)
+    # train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data,  batch_size=64)
+
+    for epoch in range(5000):
+
+        # keep track of the loss, error and the number of batches 
+        current_loss  = 0
+        current_error = 0
+        num_batches = 0 
+        acc_flag = 0
+        preds = []
+        
+        avg_loss = 0
+        
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            b_input,  b_labels = batch
+            b_input = b_input.to(torch.float32)
+            b_labels = b_labels.to(torch.float32)
+            y_pred = net(b_input)
+            y_pred = torch.nn.ReLU()(y_pred)
+            y_pred = torch.clamp(y_pred, min=0, max=1)
+            loss = loss_function(y_pred, b_labels)
+            # print(loss)
+            avg_loss += loss
+            
+            optimizer.zero_grad()
+            loss.backward()
+            # update weights
+            optimizer.step()
+            
+            logits = y_pred.detach().cpu().numpy()
+            b_labels = b_labels.detach().cpu().numpy()
+            accuracy = flat_accuracy(logits, b_labels)
+            acc_flag+=accuracy
+        
+        if (epoch + 1) % 100 == 0:
+            avg_loss_epoch = avg_loss / len(train_dataloader)
+            loss_test = test_loss(data.test_x, data.test_y)
+            print(epoch + 1, avg_loss_epoch, loss_test)
